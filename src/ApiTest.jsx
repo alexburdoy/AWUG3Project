e7eecdb25d49749e44d69be28d5d5b89a23b44ed
Alex Burdoy <alex.burdoy@estudiantat.upc.edu>
2021-02-22 11:57:00 +0100

    Primeres Consultes

--- src/ApiTest.jsx
+++ src/ApiTest.jsx
@@ -3,45 +3,52 @@ import './ApiTest.css';
 
 // https://randomuser.me/api/?results=50
 
-const URL_BASE = 'https://randomuser.me/api';
+const URL_BASE = 'https://api.rawg.io/api';
 
-const User = ({ user }) => {
+const Game = ({ game }) => {
   return (
-    <div className="user">
-      <div className="avatar">
-        <img src={user.picture.thumbnail} />
-      </div>
+    <div className="game">
       <div>
         <div className="name">
-          {user.name.first} {user.name.last}
+          {game.name}
         </div>
-        <div className="email">{user.email}</div>
+
       </div>
     </div>
   );
 };
 
-export default function ApiTest() {
-  const [users, setUsers] = useState(null);
 
-  const loadUsers = async () => {
-    const response = await fetch(`${URL_BASE}/?results=50`);
+export default function ApiTest() {
+  const [games, setGames] = useState(null);
+
+  // https://api.rawg.io/api/platforms
+  // https://api.rawg.io/api/games
+  // https://api.rawg.io/api/games?page=4&platform=18
+  // https://api.rawg.io/api/games/{id} 
+  // https://api.rawg.io/api/games/{id}/suggested
+
+
+  const loadGames = async () => {
+    const response = await fetch(`${URL_BASE}/games?`);
     const json = await response.json();
-    setUsers(json.results);
+    setGames(json.results);
   };
 
   useEffect(() => {
-    loadUsers();
+    loadGames();
   }, []);
 
-  if (users === null) {
+  if (games === null) {
     return <p>Loading...</p>;
   }
   return (
     <div>
-      {users.map((user) => (
-        <User user={user} />
+      {games.map((game) => (
+        <Game game={game} />
       ))}
     </div>
   );
+  
 }
+ 
