e09e8f591c3f72427565a8dd1c55c3bc9a636ae5
Marti Pena <penabernilsmarti@gmail.com>
2021-02-28 19:39:34 +0100

    Consultes practica 1

--- src/ApiTest.jsx
+++ src/ApiTest.jsx
@@ -12,33 +12,115 @@ const Game = ({ game }) => {
         <div className="name">
           {game.name}
         </div>
+        <div className="release">
+          Release: {game.released}
+        </div>
+        <div className="rating">
+          Rating: {game.rating}
+        </div>
 
       </div>
     </div>
   );
 };
 
+const Platforms = ({ platforms }) => {
+  return (
+    <div className="platforms">
+      <div>
+        <div className="name">
+          {platforms.name}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+const Stores = ({ stores }) => {
+  return (
+    <div className="stores">
+      <div>
+        <div className="name">
+          {stores.name} - ID: {stores.id}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+const Publishers = ({ publishers }) => {
+  return (
+    <div className="publishers">
+      <div>
+        <div className="name">
+          {publishers.name}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+const Developers = ({ developers }) => {
+  return (
+    <div className="developers">
+      <div>
+        <div className="name">
+          {developers.name}
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default function ApiTest() {
-  const [games, setGames] = useState(null);
+  const [games, setGames] = useState(null);//Mostra la primera pagina de jocs
+  const [platforms, setPlatforms] = useState(null);//Mostra la llista de plataformes(agrupades per marca)
+  const [stores, setStores] = useState(null);//Mostra la llista de tendes digitals
+  const [publishers, setPublishers] = useState(null);//Mostra la llista de empreses publicadores
+  const [developers, setDevelopers] = useState(null);//Mostra la llista de empreses desenvolupadores
 
-  // https://api.rawg.io/api/platforms
-  // https://api.rawg.io/api/games
-  // https://api.rawg.io/api/games?page=4&platform=18
-  // https://api.rawg.io/api/games/{id} 
-  // https://api.rawg.io/api/games/{id}/suggested
-  //hola
-  //prova
-  //victor prova
 
+  //buscar algo + ?page=2 per triar la pagina a buscar
   const loadGames = async () => {
     const response = await fetch(`${URL_BASE}/games?`);
     const json = await response.json();
     setGames(json.results);
   };
 
+  const loadPlatforms = async () => {
+    const response = await fetch(`${URL_BASE}/platforms/lists/parents?`);
+    const json = await response.json();
+    setPlatforms(json.results);
+  };
+
+  const loadStores = async () => {
+    const response = await fetch(`${URL_BASE}/stores?/{id:1}`);
+    const json = await response.json();
+    setStores(json.results);
+  };
+
+  const loadPublishers = async () => {
+    const response = await fetch(`${URL_BASE}/publishers?page_size=40`);
+    const json = await response.json();
+    setPublishers(json.results);
+  };
+
+  const loadDevelopers = async () => {
+    const response = await fetch(`${URL_BASE}/developers?page_size=100`);
+    const json = await response.json();
+    setDevelopers(json.results);
+  };
+
   useEffect(() => {
     loadGames();
+    loadPlatforms();
+    loadStores();
+    loadPublishers();
+    loadDevelopers();
   }, []);
 
   if (games === null) {
@@ -49,8 +131,24 @@ export default function ApiTest() {
       {games.map((game) => (
         <Game game={game} />
       ))}
+      <br></br>
+      {platforms.map((platforms) => (
+        <Platforms platforms={platforms} />
+      ))}
+      <br></br>
+      {stores.map((stores) => (
+        <Stores stores={stores} />
+      ))}
+      <br></br>
+      {publishers.map((publishers) => (
+        <Publishers publishers={publishers} />
+      ))}
+      <br></br>
+      {developers.map((developers) => (
+        <Developers developers={developers} />
+      ))}
+
     </div>
   );
-  
+
 }
- 
