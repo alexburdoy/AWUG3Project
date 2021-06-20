2a0a89d56357fca2f7f00d4d487dc3b83164ec0c
martipena <penabernilsmarti@gmail.com>
2021-06-20 11:15:50 +0200

    Petit error trending

--- components/Trending.js
+++ components/Trending.js
@@ -4,15 +4,17 @@ import React, { useContext } from "react";
 import { Text, TouchableOpacity, Image, View } from "react-native";
 import { ModelContext } from "../model/GameModel";
 import styles from "../styles";
+import { useNavigation } from '@react-navigation/native';
 
 const Trending = observer(({ trending }) => {
   const model = useContext(ModelContext);
+  const navigation = useNavigation();
   return (
     <View style={styles.person}>
       <View>
         <Text style={styles.name}>{trending.name}</Text>
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("GameDetailsPage")}
+          onPress={() => navigation.navigate("GameDetailsPage")}
         >
           <Image
             style={{
